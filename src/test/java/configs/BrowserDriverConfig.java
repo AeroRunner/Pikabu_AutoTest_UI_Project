4693adec1538ea028a98a34f9b00befdecf20b5b
AeroRunner <airplay1x6@gmail.com>
2024-02-26 13:00:57 +0300

    add static import Config*

--- src/test/java/configs/BrowserDriverConfig.java
+++ src/test/java/configs/BrowserDriverConfig.java
@@ -1,30 +1,34 @@
 package configs;
 
-import  org.aeonbits.owner.Config;
+
+import org.aeonbits.owner.Config;
+
+import static org.aeonbits.owner.Config.*;
 
 
-@Config.Sources({"classpath:properties/${host}.properties"})
+
+@Sources({"classpath:properties/${host}.properties"})
 public interface BrowserDriverConfig extends Config {
 
 
-    @Config.Key("browser")
-    @Config.DefaultValue("CHROME")
+    @Key("browser")
+    @DefaultValue("CHROME")
     String getBrowser();
 
-    @Config.Key("browserSize")
-    @Config.DefaultValue("1920x1080")
+    @Key("browserSize")
+    @DefaultValue("1920x1080")
     String getBrowserSize();
 
-    @Config.Key("version")
-    @Config.DefaultValue("100")
+    @Key("version")
+    @DefaultValue("100")
     String getBtowserVersion();
 
-    @Config.Key("remoteURL")
-    @Config.DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub/")
+    @Key("remoteURL")
+    @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub/")
     String getRemoteURL();
 
-    @Config.Key("isRemote")
-    @Config.DefaultValue("false")
+    @Key("isRemote")
+    @DefaultValue("false")
     Boolean isRemote();
 
 }
